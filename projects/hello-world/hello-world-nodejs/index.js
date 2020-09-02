eee7c0f7ad3d7d153dce94d2452c3519902bf185
jahigueram <jahigueram@outlook.com>
2020-09-02 17:04:20 -0500

    Se cambia el comando CMD por ENTRYPOINT

--- projects/hello-world/hello-world-nodejs/index.js
+++ projects/hello-world/hello-world-nodejs/index.js
@@ -2,14 +2,14 @@ var express = require('express')
 const process = require('process');
 
 var expressapp = express()
-expressapp.get('/', function (req, res) {
-    res.send('{"message":"Hello World JavaScript v1"}')
+expressapp.get('/', function(req, res) {
+    res.send('{"message":"Hello World JavaScript v2.1"}')
 })
-expressapp.listen(5000, function () {
+expressapp.listen(5000, function() {
     console.log('Ready on port 5000!')
 })
 
 /* To Handle Ctrl C*/
-process.on('SIGINT', function () {
+process.on('SIGINT', function() {
     process.exit();
 });
